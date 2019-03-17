1c967f501c394d3ec5f9f8a29224ed363034fa6b
Tiffany Marroquin <tiffany.marroq@gmail.com>
2019-03-17 09:46:38 -0400

    Initial dist subtree commit

---
+++ precache-manifest.49de9dc98b59494efcbf1838612daea2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "ea8e4b5407155f14fb0b222b39824658",
+    "url": "/static/media/UniqloProRegular.ea8e4b54.ttf"
+  },
+  {
+    "revision": "538106203ef20ff72d001cec96409479",
+    "url": "/static/media/UniqloProBold.53810620.ttf"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e4427a6a912e376675fc",
+    "url": "/static/js/main.5e4796a1.chunk.js"
+  },
+  {
+    "revision": "724ae3d591e5849b7d1d",
+    "url": "/static/js/2.48e61b72.chunk.js"
+  },
+  {
+    "revision": "e4427a6a912e376675fc",
+    "url": "/static/css/main.d33cfeec.chunk.css"
+  },
+  {
+    "revision": "724ae3d591e5849b7d1d",
+    "url": "/static/css/2.ff21222f.chunk.css"
+  },
+  {
+    "revision": "1fb20ead6c7cd83fd1aea2e52344394a",
+    "url": "/index.html"
+  }
+];
