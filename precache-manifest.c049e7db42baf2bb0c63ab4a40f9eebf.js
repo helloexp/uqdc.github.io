7786bfbc7f9fd4ce9a3355565807086af910e478
Tiffany Marroquin <tiffany.marroq@gmail.com>
2019-03-18 20:02:20 -0400

    added btn styling

---
+++ precache-manifest.c049e7db42baf2bb0c63ab4a40f9eebf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "ea8e4b5407155f14fb0b222b39824658",
+    "url": "/static/media/UniqloProRegular.ea8e4b54.ttf"
+  },
+  {
+    "revision": "538106203ef20ff72d001cec96409479",
+    "url": "/static/media/UniqloProBold.53810620.ttf"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "22d0975ba3f44fcdb318",
+    "url": "/static/js/main.f1eba9b9.chunk.js"
+  },
+  {
+    "revision": "de279e50f6b0d17fa030",
+    "url": "/static/js/2.bd9b3aac.chunk.js"
+  },
+  {
+    "revision": "22d0975ba3f44fcdb318",
+    "url": "/static/css/main.d33cfeec.chunk.css"
+  },
+  {
+    "revision": "de279e50f6b0d17fa030",
+    "url": "/static/css/2.ff21222f.chunk.css"
+  },
+  {
+    "revision": "4cfb870d3b0dcb110f8c3bfaef68fea1",
+    "url": "/index.html"
+  }
+];
